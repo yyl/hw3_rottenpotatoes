8570a18a5b4c2ec162c79a6bd0b19e4ceed5442f
yyl <yyl.dev@gmail.com>
2012-06-14 15:02:22 +0000

    hw3 pt1 finished locally

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    if Movie.find_by_title(movie['title']) == nil
+      Movie.create!(movie)
+    end
   end
-  flunk "Unimplemented"
+# flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
